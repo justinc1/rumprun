1a26855fb5dd3e969a08d93b210fe4ee384f8c5b
Hajime Tazaki <thehajime@gmail.com>
2015-11-26 13:38:08 +0900

    nolibc: invoke qemu directry instead of rumprun
    kernonly build doesn't generate rumprun
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- tests/nolibc/test-nolibc.sh
+++ tests/nolibc/test-nolibc.sh
@@ -21,15 +21,21 @@ fi
 
 cd $(dirname $0)
 echo "=== test main.elf ==="
-rumprun qemu -M 512 -g "-nographic -vga none" -i main.elf </dev/null &> log.txt &
+qemu-system-x86_64 -net nic,model=virtio -no-kvm -m 512 -kernel main.elf -s -nographic -vga none </dev/null &> log.txt &
+#rumprun qemu -M 512 -g "-nographic -vga none" -i main.elf </dev/null &> log.txt &
 sleep 3
-kill %1 || echo "ignore errors"
+pkill qemu || echo "ignore errors"
 cat log.txt
 
 echo "=== test main-net.elf ==="
 rm -f log.txt
-rumprun qemu -M 512 -g "-net nic,model=virtio -net tap,script=no,vlan=0,ifname=tap0 -nographic -vga none" \
-	-i main-net.elf </dev/null &> log.txt &
+#rumprun qemu -M 512 -g "-net nic,model=virtio -net tap,script=no,vlan=0,ifname=tap0 -nographic -vga none" \
+#	-i main-net.elf </dev/null &> log.txt &
+sudo ip tuntap add tap0 mode tap
+sudo ip ad add 10.0.0.2/24 dev tap0
+sudo ifconfig tap0 up
+qemu-system-x86_64 -net nic,model=virtio -net tap,script=no,vlan=0,ifname=tap0 -no-kvm -m 512 -kernel main-net.elf -s -nographic -vga none </dev/null &> log.txt &
+
 sleep 3
 pkill qemu || echo "ignore errors"
 cat log.txt
