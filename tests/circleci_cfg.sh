016ef1b0b6c1446cd8946eebd4dbb0e1d3f95bf9
Hajime Tazaki <thehajime@gmail.com>
2016-05-23 23:13:57 +0900

    test netbsd/linux with w/ and w/o kernonly flag
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- tests/circleci_cfg.sh
+++ tests/circleci_cfg.sh
@@ -8,35 +8,40 @@
 
 case $CIRCLE_NODE_INDEX in
     0)
-	echo "hw"
+	echo "hw-linux"
 	echo "export PLATFORM=hw; export MACHINE=x86_64; export TESTS=qemu;\
- export KERNONLY=; export EXTRAFLAGS=; export LINUX='-r linux'" >> $HOME/.bashrc
+ export KERNONLY=; export EXTRAFLAGS=; export RUMPKERN='-r linux'" >> $HOME/.bashrc
 	;;
     1)
-	echo "hw i486"
-	echo "export PLATFORM=hw; export MACHINE=i486; export ELF=elf; export TESTS=qemu;\
- export KERNONLY=; export EXTRAFLAGS='-- -F ACLFLAGS=-m32 -F ACLFLAGS=-march=i686'; export LINUX='-r linux'" >> $HOME/.bashrc
+	echo "hw-netbsd"
+	echo "export PLATFORM=hw; export MACHINE=x86_64; export TESTS=qemu;\
+ export KERNONLY=; export EXTRAFLAGS=; export RUMPKERN='-r netbsd'" >> $HOME/.bashrc
 	;;
     2)
-	echo "hw kernonly"
+	echo "hw kernonly-linux"
 	echo "export PLATFORM=hw; export MACHINE=x86_64; export TESTS=none;\
-export KERNONLY=-k; export EXTRAFLAGS=; export LINUX='-r linux'" >> $HOME/.bashrc
+export KERNONLY=-k; export EXTRAFLAGS=; export RUMPKERN='-r linux'" >> $HOME/.bashrc
 	;;
     3)
-	echo "xen kernonly"
-	echo "export PLATFORM=xen; export MACHINE=x86_64; export TESTS=none;\
- export KERNONLY=-k; export EXTRAFLAGS=; export LINUX='-r linux'" >> $HOME/.bashrc
+	echo "hw kernonly-netbsd"
+	echo "export PLATFORM=hw; export MACHINE=x86_64; export TESTS=none;\
+ export KERNONLY=-k; export EXTRAFLAGS=; export RUMPKERN='-r netbsd'" >> $HOME/.bashrc
 	;;
     # The followings are not yet supported for Linux kernel build
     4)
 	echo "xen"
 	echo "export PLATFORM=xen; export MACHINE=x86_64; export TESTS=none;\
- export KERNONLY=; export EXTRAFLAGS=; export LINUX='-r linux'" >> $HOME/.bashrc
+ export KERNONLY=; export EXTRAFLAGS=; export RUMPKERN='-r linux'" >> $HOME/.bashrc
 	;;
     5)
 	echo "xen i486"
 	echo "export PLATFORM=xen; export MACHINE=i486; export ELF=elf; export TESTS=none;\
- export KERNONLY=; export EXTRAFLAGS='-- -F ACLFLAGS=-m32'; export LINUX='-r linux'" >> $HOME/.bashrc
+ export KERNONLY=; export EXTRAFLAGS='-- -F ACLFLAGS=-m32'; export RUMPKERN='-r linux'" >> $HOME/.bashrc
+	;;
+    6)
+	echo "hw i486"
+	echo "export PLATFORM=hw; export MACHINE=i486; export ELF=elf; export TESTS=qemu;\
+ export KERNONLY=; export EXTRAFLAGS='-- -F ACLFLAGS=-m32 -F ACLFLAGS=-march=i686'; export RUMPKERN='-r linux'" >> $HOME/.bashrc
 	;;
 esac
 
